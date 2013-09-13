803f3966dd81691edb7e0c90cfa9734baea9f316
Mark Hatton <mark@zeebox.com>
2013-09-13 19:09:38 +0100

    carry around a list<string> and use insertion sort

--- src/anagrams.cpp
+++ src/anagrams.cpp
@@ -11,8 +11,15 @@
 
 using namespace std;
 
+
 set<string> dict;
-void solve(const string w, const string remain, const string acc);
+
+void solve(const string w, const string remain, const list<string> acc);
+
+inline list<string> insertionSort(const list<string> xs, const string x);
+
+inline string makeString(list<string> xs);
+
 
 void usage(string message = "")
 {
@@ -70,27 +77,39 @@ int main(int argc, char* argv[])
     dict.insert("ab");
     dict.insert("bc");
 
-    solve("", input, "");
+    solve("", input, list<string>());
 }
 
 set<string> partials;
 
-string buildPartial(string s)
+inline list<string> insertionSort(const list<string> xs, const string x)
 {
-    list<string> sorted;
-    istringstream iss(s);
-    copy(istream_iterator<string>(iss),
-        istream_iterator<string>(),
-        back_inserter<list<string> >(sorted));
-    sorted.sort();
-
-    stringstream ss;
+    list<string> sorted = xs;
     for (list<string>::iterator it=sorted.begin(); it!=sorted.end(); ++it)
-        ss << *it << " ";
+    {
+        if (x < *it)
+        {
+            sorted.insert(it, x);
+            break;
+        }
+    }
+    if (xs.size() == sorted.size())
+        sorted.push_back(x);
+    return sorted;
+}
+
+inline string makeString(list<string> xs)
+{
+    stringstream ss;
+    for (list<string>::iterator it=xs.begin(); it!=xs.end(); ++it)
+        if (it == xs.begin())
+            ss << *it;
+        else
+            ss << ' ' << *it;
     return ss.str();
 }
 
-void solve(const string w, const string remain, const string acc)
+void solve(const string w, const string remain, const list<string> acc)
 {
     for (unsigned i=0; i < remain.length(); ++i)
     {
@@ -98,15 +117,17 @@ void solve(const string w, const string remain, const string acc)
         string w_ = w + c;
         string remain_ = remain.substr(0, i) + remain.substr(i + 1, remain.length() - i - 1);
 
-        if (dict.count(w_) > 0) {
-            string p = buildPartial(acc + w_);
+        if (dict.count(w_) > 0)
+        {
+            list<string> partial = insertionSort(acc, w_);
+            string p = makeString(partial);
             if (partials.count(p) == 0)
             {
                 partials.insert(p);
                 if (remain_.empty())
-                    cout << acc + w_ << endl;
+                    cout << p << endl;
                 else
-                    solve("", remain_, p);
+                    solve("", remain_, partial);
             }
         }
 
