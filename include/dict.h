e67238ddd6f8c2408e5ae419a9f4bb92cf9aafcc
Mark Hatton <mark@zeebox.com>
2013-09-14 15:18:05 +0100

    load unigram freqs from dictionary if available

--- include/dict.h
+++ include/dict.h
@@ -23,6 +23,7 @@ void loadDictionary(const string &unigramsFile)
 {
     string line;
     string word;
+    string freq;
     ifstream infile;
     infile.open(unigramsFile.c_str(), ifstream::in);
 
@@ -32,10 +33,12 @@ void loadDictionary(const string &unigramsFile)
 
         stringstream ss(line);
         getline(ss, word, '\t');
+        if (!getline(ss, freq, '\t'))
+            freq = "1";
 
         if (word.length() > 0) {
-            // cout << "adding " << word << " to dict, with len " << word.length() << endl;
-            dict[word] = 1;
+            // cout << "adding " << word << " to dict, with freq. " << freq << endl;
+            dict[word] = atol(freq.c_str());
         }
     }
 
