a75bdd63f622501d5cbeb99a4fb46c578d0e7d57
Mark Hatton <mark@zeebox.com>
2013-09-14 16:48:39 +0100

    hack to reduce # of short words (TODO this should be captured in input files only)

--- include/dict.h
+++ include/dict.h
@@ -19,6 +19,86 @@ typedef trie_map<string, long, 32> trie_t;
 
 trie_t dict;
 
+string short_words_arr[] = {
+    "a",
+    "i",
+    "ab",
+    "ad",
+    "ah",
+    "am",
+    "an",
+    "as",
+    "at",
+    "be",
+    "bi",
+    "bo",
+    "by",
+    "co",
+    "da",
+    "de",
+    "do",
+    "ed",
+    "eh",
+    "el",
+    "em",
+    "er",
+    "ex",
+    "go",
+    "ha",
+    "he",
+    "hi",
+    "ho",
+    "id",
+    "if",
+    "in",
+    "is",
+    "it",
+    "kg",
+    "km",
+    "la",
+    "lb",
+    "lo",
+    "ma",
+    "me",
+    "ml",
+    "mm",
+    "ms",
+    "my",
+    "na",
+    "no",
+    "of",
+    "oh",
+    "oi",
+    "ol",
+    "on",
+    "op",
+    "or",
+    "ox",
+    "pa",
+    "pi",
+    "pm",
+    "po",
+    "pi",
+    "pm",
+    "po",
+    "re",
+    "so",
+    "st",
+    "ta",
+    "to",
+    "uh",
+    "um",
+    "un",
+    "up",
+    "us",
+    "vs",
+    "we",
+    "ya",
+    "ye",
+    "yo"
+};
+set<string> short_words(short_words_arr, short_words_arr + 76);
+
 void loadDictionary(const string &unigramsFile)
 {
     string line;
@@ -36,8 +116,8 @@ void loadDictionary(const string &unigramsFile)
         if (!getline(ss, freq, '\t'))
             freq = "1";
 
-        if (word.length() > 0) {
-            // cout << "adding " << word << " to dict, with freq. " << freq << endl;
+        if (word.length() > 0 && (word.size() > 2 || short_words.count(word) > 0)) {
+            // cout << "adding " << word << " to dict, with len " << word.length() << endl;
             dict[word] = atol(freq.c_str());
         }
     }
